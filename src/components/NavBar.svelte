3e3b0c0088c0d52dd872f708b267ead477d80a5f
Codecrafter_404 <codecrafter404@github.com>
2024-07-07 00:29:41 +0200

    added the professional printer css & started working on the css switch button for print

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-    import {apply_color_scheme, is_darkmode, switch_darkmode} from "../lib/ColorUtil";
+    import {
+        apply_color_scheme,
+        is_darkmode,
+        switch_darkmode,
+    } from "../lib/ColorUtil";
     import { normalize_path } from "../lib/FileUtils";
     let darkmode = is_darkmode();
     function darkmode_switch() {
@@ -7,15 +11,15 @@
         apply_color_scheme();
     }
     let path = normalize_path(document.location.pathname);
-        path = `root${path}`
+    path = `root${path}`;
     let segments = path.split("/");
     function navigate(event: PointerEvent) {
         let target = event.target as HTMLSpanElement;
         let i = +(target.getAttribute("pos") || "0");
-            i += 1;
+        i += 1;
         let path = segments.slice(0, i).join("/");
-            path = path.substring("root".length)
-        if(path === "") path = "/";
+        path = path.substring("root".length);
+        if (path === "") path = "/";
         document.location.pathname = path;
     }
 </script>
@@ -24,29 +28,59 @@
     class="w-full flex justify-between px-10 py-1 bg-secondary-background items-center nav-bar"
 >
     <div class="">
-        <img src="/logo.svg" alt="" class="max-h-[4rem]"/>
+        <img src="/logo.svg" alt="" class="max-h-[4rem]" />
     </div>
     <div class="">
         {#each segments as segment, i}
             <span>/</span>
-            <span class="text-primary-accent cursor-pointer hover:underline" pos={i} on:click={navigate}>{segment}</span>
+            <span
+                class="text-primary-accent cursor-pointer hover:underline"
+                pos={i}
+                on:click={navigate}>{segment}</span
+            >
             <span> </span>
         {/each}
     </div>
-    <div>
-        <button class="p-2 rounded-[.25rem] button bg-opacity-10" id="button" on:click={darkmode_switch}>
+    <div class="flex space-x-2">
+        <div class="">
+            <button
+                class="p-2 rounded-[.25rem] button bg-opacity-10 button"
+            >
+                <div
+                    class="scheme-icon"
+                    style="--icon: url({'/icons/printer.svg'})"
+                ></div>
+            </button>
+            <div
+                class="absolute origin-bottom-left  left-0 z-10 bg-primary-background rounded-md ring-primary-accent ring-2 m-4 p-2"
+            >
+                asdf asdf asdf asdf
+                <ul>
+                    <li>asdf</li>
+                    <li>asdf</li>
+                    <li>asdf</li>
+                    <li>asdf</li>
+                    <li>asdf</li>
+                    <li>asdf</li>
+                </ul>
+            </div>
+        </div>
+        <button
+            class="p-2 rounded-[.25rem] button bg-opacity-10"
+            on:click={darkmode_switch}
+        >
             <div
                 style="--icon: url({darkmode
                     ? '/icons/color-scheme/moon.svg'
                     : '/icons/color-scheme/sun.svg'})"
-                id="scheme-icon"
+                class="scheme-icon"
             ></div>
         </button>
     </div>
 </div>
 
 <style lang="scss">
-    #button {
+    .button {
         --transparency: 6.25%;
         background-color: color-mix(
             in srgb,
@@ -54,7 +88,7 @@
             transparent
         );
     }
-    #button:hover {
+    .button:hover {
         --transparency: 12.5%;
         background-color: color-mix(
             in srgb,
@@ -62,7 +96,7 @@
             transparent
         );
     }
-    #scheme-icon {
+    .scheme-icon {
         background-color: var(--primary-accent-color);
         mask-image: var(--icon); //var(--icon);
         mask-size: contain;
